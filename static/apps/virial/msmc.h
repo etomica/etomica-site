a5a8ecde7f7e28367040fc7c9a6a3fca551cad14
Andrew Schultz <ajs42@buffalo.edu>
2022-05-05 05:20:50 -0400

    Remove unused file

--- static/apps/virial/msmc.h
+++
@@ -1,5 +0,0 @@
-
-void setParameters(int nop, double T, unsigned long rseed, int nDer, double sigmaHS);
-double runTargetStepSize(int numSteps);
-double runAlpha(int numSteps);
-int runProduction(int numSteps);
