26d19c0f6240ee56fb1795fe4e111b99d5b57251
Andrew Schultz <ajs42@buffalo.edu>
2022-05-12 09:30:46 -0400

    Whitespace cleanup

--- static/apps/virial/emscripten.js
+++ static/apps/virial/emscripten.js
@@ -20,39 +20,39 @@ var Module = {
     } else {
       console.error(text);
     }
-    },
-      setStatus: function(text) {
-        if (!Module.setStatus.last) Module.setStatus.last = { time: Date.now(), text: '' };
-        if (text === Module.setStatus.text) return;
-        var m = text.match(/([^(]+)\((\d+(\.\d+)?)\/(\d+)\)/);
-            var now = Date.now();
-            if (m && now - Date.now() < 150) return; // if this is a progress update, skip it if too soon
-            if (m) {
-              text = m[1];
-              progressElement.value = parseInt(m[2])*100;
-              progressElement.max = parseInt(m[4])*100;
-              progressElement.hidden = false;
-              spinnerElement.hidden = false;
-            } else {
-              progressElement.value = null;
-              progressElement.max = null;
-              progressElement.hidden = true;
-              if (!text) spinnerElement.hidden = true;
-            }
-            statusElement.innerHTML = text;
-            },
-            totalDependencies: 0,
-            monitorRunDependencies: function(left) {
-              this.totalDependencies = Math.max(this.totalDependencies, left);
-              Module.setStatus(left ? 'Preparing... (' + (this.totalDependencies-left) + '/' + this.totalDependencies + ')' : 'All downloads complete.');
-                }
-                };
-                Module.setStatus('Downloading...');
-                window.onerror = function() {
-                  Module.setStatus('Exception thrown, see JavaScript console');
-                  spinnerElement.style.display = 'none';
-                  Module.setStatus = function(text) {
-                    if (text) Module.printErr('[post-exception status] ' + text);
-                  };
-                };
+  },
+  setStatus: function(text) {
+    if (!Module.setStatus.last) Module.setStatus.last = { time: Date.now(), text: '' };
+    if (text === Module.setStatus.text) return;
+    var m = text.match(/([^(]+)\((\d+(\.\d+)?)\/(\d+)\)/);
+    var now = Date.now();
+    if (m && now - Date.now() < 150) return; // if this is a progress update, skip it if too soon
+    if (m) {
+      text = m[1];
+      progressElement.value = parseInt(m[2])*100;
+      progressElement.max = parseInt(m[4])*100;
+      progressElement.hidden = false;
+      spinnerElement.hidden = false;
+    } else {
+      progressElement.value = null;
+      progressElement.max = null;
+      progressElement.hidden = true;
+      if (!text) spinnerElement.hidden = true;
+    }
+    statusElement.innerHTML = text;
+  },
+  totalDependencies: 0,
+  monitorRunDependencies: function(left) {
+    this.totalDependencies = Math.max(this.totalDependencies, left);
+    Module.setStatus(left ? 'Preparing... (' + (this.totalDependencies-left) + '/' + this.totalDependencies + ')' : 'All downloads complete.');
+  }
+};
+Module.setStatus('Downloading...');
+window.onerror = function() {
+  Module.setStatus('Exception thrown, see JavaScript console');
+  spinnerElement.style.display = 'none';
+  Module.setStatus = function(text) {
+    if (text) Module.printErr('[post-exception status] ' + text);
+  };
+};
 
